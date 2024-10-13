14b71342a22a6cc98500259c399ee084f37b2f15
Ahmad Searcy <ahmadsearcy7@gmail.com>
2024-10-13 01:26:18 -0500

    boiler plate code

---
+++ src/components/Nav.tsx
@@ -0,0 +1,16 @@
+import { PiShoppingCartThin } from "react-icons/pi";
+
+export const Nav: React.FC = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+            <h2 className=" text-blue-400">hello</h2>
+          <li>
+            <PiShoppingCartThin className="" />
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
