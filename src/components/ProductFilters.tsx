7a3d32fd182cb8271a3ae86dab2681b51aff71a1
Ahmad Searcy <ahmadsearcy7@gmail.com>
2024-11-05 13:35:38 -0600

    starting product filters

---
+++ src/components/ProductFilters.tsx
@@ -0,0 +1,15 @@
+
+const ProductFilters:React.FC = ()=>{
+
+    return(
+        <>
+        <div className=" flex flex-col" id="product-filters">
+        <div id="category-filter">
+            
+        </div>
+        </div>
+        </>
+    );
+}
+
+export default ProductFilters;
